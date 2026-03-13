e600fe83614ad7feed64a7c05dd3e15e455de5c7
Nicolas Ortiz <nortiz@iadb.org>
2026-03-13 09:01:52 -0300

    add dotenv and fix calling to apexRestResources

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "public")))
+//app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(__dirname))
 
 app.post("/loan-request", async (req, res) => {
   const data = req.body
--- services/salesforce.js
+++ services/salesforce.js
@@ -1,34 +1,55 @@
 const fetch = require("node-fetch")
+require("dotenv").config()
 
-const SF_BASE = "https://gl1765454964150.my.salesforce.com/services/apexrest"
+const SF_TOKEN_URL = process.env.SF_TOKEN_URL
+const SF_BASE = process.env.SF_BASE_URL
+const CLIENT_ID = process.env.SF_CLIENT_ID
+const CLIENT_SECRET = process.env.SF_CLIENT_SECRET
+
+let cachedToken = null
+
+async function getAccessToken() {
+  if (cachedToken) return cachedToken
+
+  const response = await fetch(SF_TOKEN_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
+    })
+  })
+
+  const data = await response.json()
+  cachedToken = data.access_token
+  return cachedToken
+}
 
 async function createLoanRequest(data) {
-  const response = await fetch(
-    `${SF_BASE}/loanrequest`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-
+  const token = await getAccessToken()
+  const response = await fetch(`${SF_BASE}/loanrequest`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify(data)
+  })
   return response.json()
 }
 
 async function checkEligibility(data) {
-  const response = await fetch(
-    `${SF_BASE}/loan-eligibility`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-
+  const token = await getAccessToken()
+  const response = await fetch(`${SF_BASE}/loan-eligibility`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify(data)
+  })
   return response.json()
 }
 
-module.exports = {
-  createLoanRequest,
-  checkEligibility
-}
+module.exports = { createLoanRequest, checkEligibility }
